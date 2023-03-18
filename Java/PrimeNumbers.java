d2f78fa7b350f67dd144f829721454820db44eb4
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-03-18 15:25:51 +0530

    Create

---
+++ Java/PrimeNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrimeNumber{
+    public static void main(String[] args) {
+        primeNums(new Scanner(System.in).nextInt());
+    }
+    static void primeNums(int num) {
+        int zeros;
+        for (int i=2; i<= num; i++) {
+            zeros= 0;
+            for (int j = 2; j<=i/2; j++) {
+                if (i%j == 0) {
+                    zeros++;
+                }
+            }
+            if (zeros == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
